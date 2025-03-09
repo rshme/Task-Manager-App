b01beb29bc6fd7eba018ba2334ad745e3c81ecbe
Rafi Septian Hadi <rafiseptian90@gmail.com>
2025-03-09 16:15:52 +0700

    docs: Add README.md

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { AppState } from "react-native";
 import TaskModel from "../models/Task";
 import TaskView from "../views/TaskView";
 
 const TaskController = () => {
   const [tasks, setTasks] = useState(TaskModel.getTasks());
+  const [appState, setAppState] = useState(AppState.currentState);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+      console.log("App state changed:", nextAppState);
+      setAppState(nextAppState);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
 
   const addTask = (task) => {
     TaskModel.addTask(task);
